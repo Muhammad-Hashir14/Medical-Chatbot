4a2f60f11fc9e59a1690ae313d0bf05cf0b931d7
Muhammad Hashir <hashirgohar@gmail.com>
2025-07-13 23:58:40 +0500

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="MedicalBot",
+    author ="Muhammad Hashir", 
+    version = "0.0.0",
+    author_email = "hashirgohar@gmail.com",
+    packages =  find_packages(),
+    install_requires = []
+)
---
+++ template.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import logging
+import os
+
+files = [
+    "src/__init.py__",
+    "src/helper.py",
+    "src/prompt.py",
+    "research/snippets.ipynb",
+    "setup.py",
+    ".env",
+    "requirements.txt",
+    "app.py"
+]
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+for filepath in files:
+    filepath = Path(filepath)
+    dir, file = os.path.split(filepath)
+
+    if dir != "":
+        os.makedirs(dir, exist_ok=True)
+        logging.info(f"Creating directory: {dir} for the file: {file}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    
+    else:
+        logging.info(f"File already exist: {file}")
